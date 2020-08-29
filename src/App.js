503153e2960f18bd0c9f1e74d0852396b60a8680
DaehunKim <gidonskaris@gmail.com>
2020-08-29 10:00:17 +0900

    [Add] Fetch API

--- src/App.js
+++ src/App.js
@@ -4,60 +4,83 @@ import Movie from "./components/Movie";
 
 
 class App extends Component {
+    state = {}
 
-    // 상태 선언
-    state = {} //초기값은 비어있다.
-    // 함수 선언
-    renderMovie = () => {
+    componentDidMount(){
+        this._getMovies();
+    }
+
+    _renderMovies = () => {
         const movies = this.state.movies.map(movie => {
-            return (
-                <Movie poster={movie.poster} title={movie.title} />
-            )
+            return <Movie
+                title={movie.title}
+                poster={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                key={movie.id}
+
+            />
         })
         return movies
     }
-    // 라이프사이클 함수 선언  -> 일련의 탄생부터 죽음까지의 생명주기
-    // Render: componentWillMount() -> render() -> componentDidMount()
-    // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-    componentDidMount() { //마운트 되고 나서 실행되는 함수 무조건 -  데이터 불러올 때나 토큰 체크를 할때
-        setTimeout(() => {
-            this.setState({
-                movies : [
-                    {
-                        title: "Trainspotting",
-                        poster: "https://resizing.flixster.com/OUEArjor-MbyCV6GqLU85Hk9jQI=/300x300/v1.bjs1NTQ3OTM7ajsxNzQ2NjsxMjAwOzIwMDA7MTMzMQ"
-                    },
-                    {
-                        title: "Matrix",
-                        poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
-                    },
-                    {
-                        title: "Full Metal Jacket",
-                        poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-                    },
-                    {
-                        title: "Old boy",
-                        poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
-                    },
-                    {
-                        title: "Star Wars",
-                        poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-                    }
-
-                ]
-            })
-        }, 5000) //5초 후에 위의 데이터를 넣겠다.
+    _getMovies =  async () => {
+        const movies = await this._callApi()
+        this.setState({
+            movies
+        })
     }
 
-
-
+    _callApi = () => {
+        return fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US&page=1')
+            .then(potato => potato.json())
+            .then(json => json.postInfo)
+            .catch(err => console.log(err))
+    }
+    // // 상태 선언
+    // state = {
+    //     movies: []
+    // } //초기값은 항상은 아니지만 여기선 비어있는 상태.
+    // // 함수 선언
+    //
+    // componentDidMount() { //마운트 되고 나서 실행되는 함수 무조건 -  데이터 불러올 때나 토큰 체크를 할때
+    //     this.getMovies()
+    // }
+    //
+    //
+    // renderMovie = () => {
+    //     const movies = this.state.movies.map(movie => {
+    //         return (
+    //             <Movie poster={movie.small_cover_image} title={movie.title} />
+    //         )
+    //     })
+    //     return movies
+    // }
+    //
+    // getMovies = async () => {
+    //     const movies = await this.callAPI()
+    //     this.setState({
+    //         movies
+    //     })
+    // }
+    // // 라이프사이클 함수 선언  -> 일련의 탄생부터 죽음까지의 생명주기
+    // // Render: componentWillMount() -> render() -> componentDidMount()
+    // // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+    //
+    //
+    //
+    // callAPI = () => {
+    //    return fetch("http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-29&sortBy=publishedAt&apiKey=f1c87e2bb86248de9f9492e513f93e1f")
+    //         .then(data => data.json())
+    //         .then(json => json.articles)
+    //         .then(movies => console.log(movies))
+    //         .catch(err => console.log(err))
+    // }
+    //
     render() {
         //위의 것들을 다시 재선언
-        const {movies, greeting} = this.state
+        const {movies} = this.state
         return (
             <div className={"App"}>
-                {movies ? this.renderMovie() : "Loading"}
+                {movies ? this._renderMovies() : "Loading"}
             </div>
         );
     }
